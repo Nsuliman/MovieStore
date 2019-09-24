40d15ef5d9412a4a8652299f1acdce832cb8b3a3
Nsuliman <nsuliman88@gmail.com>
2019-09-24 18:03:58 +0300

    java script

---
+++ java/java.js
@@ -0,0 +1,16 @@
+var today = new Date();
+var hournow = prompt(" Please enter what the is now : ");
+var greeting;
+
+if (hournow > 18 ) {
+    greeting = ' Good Evening' ;
+} else if (hournow > 12) {
+    greeting = ' Good Afternoon' ;
+} else if (hournow >= 0) {
+    greeting = ' Good moring' ;
+
+} else {
+    greeting
+    
+    confirm (" Press a Button ")
+    document.write('<h3>' + greeting + '</h3>');
